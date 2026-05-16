b2e30e04e8d42a467f68a7afef02754f3ccb27a4
Lakshmi Venkata Sai Raj Abhinav. Thotapalli <thotapalli.abhinav14@gmail.com>
2026-05-16 15:08:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from model_router import local_chat, groq_chat
 # ==============================
 st.set_page_config(page_title="Chatbot")
 
-st.title("SUBHASHINI")
+st.title("BUDDY")
 
 # ==============================
 # DETECT CLOUD ENVIRONMENT
